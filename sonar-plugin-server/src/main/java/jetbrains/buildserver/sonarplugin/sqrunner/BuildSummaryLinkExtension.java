a4ac278c82eb7bc4b5cae6bd695fb40eaba641a4
Linfar <tivof_andrey@mail.ru>
2014-04-08 15:12:48 +0400

    Improving SonarQube link on the build summary page - now is readed from the artifact

--- sonar-plugin-server/src/main/java/jetbrains/buildserver/sonarplugin/sqrunner/BuildSummaryLinkExtension.java
+++ sonar-plugin-server/src/main/java/jetbrains/buildserver/sonarplugin/sqrunner/BuildSummaryLinkExtension.java
@@ -3,6 +3,7 @@ package jetbrains.buildserver.sonarplugin.sqrunner;
 import jetbrains.buildServer.controllers.BuildDataExtensionUtil;
 import jetbrains.buildServer.serverSide.SBuild;
 import jetbrains.buildServer.serverSide.SBuildServer;
+import jetbrains.buildServer.serverSide.artifacts.BuildArtifact;
 import jetbrains.buildServer.serverSide.artifacts.BuildArtifactHolder;
 import jetbrains.buildServer.serverSide.artifacts.BuildArtifactsViewMode;
 import jetbrains.buildServer.web.openapi.PlaceId;
@@ -10,16 +11,24 @@ import jetbrains.buildServer.web.openapi.PluginDescriptor;
 import jetbrains.buildServer.web.openapi.SimplePageExtension;
 import jetbrains.buildServer.web.openapi.WebControllerManager;
 import jetbrains.buildserver.sonarplugin.Constants;
+import jetbrains.buildserver.sonarplugin.Util;
 import org.jetbrains.annotations.NotNull;
 
 import javax.servlet.http.HttpServletRequest;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Map;
 
 /**
  * Created by linfar on 4/4/14.
- *
+ * <p>
  * Link to the SonarQube Server on the Build Summary table
+ * </p>
  */
 public class BuildSummaryLinkExtension extends SimplePageExtension {
+    public static final long ABSOLUTE_FILESIZE_THRESHOLD = 1000L;
     @NotNull
     private final SBuildServer myServer;
 
@@ -32,8 +41,41 @@ public class BuildSummaryLinkExtension extends SimplePageExtension {
     }
 
     @Override
-    public boolean isAvailable(@NotNull HttpServletRequest request) {
-        SBuild build = BuildDataExtensionUtil.retrieveBuild(request, myServer);
+    public void fillModel(@NotNull Map<String, Object> model, @NotNull HttpServletRequest request) {
+        final SBuild build = BuildDataExtensionUtil.retrieveBuild(request, myServer);
+        if (build != null) {
+            final BuildArtifact artifact = build.getArtifacts(BuildArtifactsViewMode.VIEW_HIDDEN_ONLY).getArtifact(Constants.SONAR_SERVER_URL_ARTIF_LOCATION_FULL);
+            if (artifact != null) {
+                if (artifact.getSize() > ABSOLUTE_FILESIZE_THRESHOLD) {
+                    model.put("sonar_bigUrlFile", artifact.getSize());
+                } else {
+                    InputStream inputStream = null;
+                    try {
+                        inputStream = artifact.getInputStream();
+                        String url = readUrlFromStream(inputStream);
+                        model.put("sonar_url", url);
+                    } catch (IOException e) {
+                        model.put("sonar_IOException", e);
+                    } finally {
+                        Util.close(inputStream);
+                    }
+                }
+            } else {
+                model.put("sonar_noArtifact", Boolean.TRUE);
+            }
+        }
+        super.fillModel(model, request);
+    }
+
+    @NotNull
+    protected String readUrlFromStream(@NotNull final InputStream inputStream) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+        return reader.readLine();
+    }
+
+    @Override
+    public boolean isAvailable(@NotNull final HttpServletRequest request) {
+        final SBuild build = BuildDataExtensionUtil.retrieveBuild(request, myServer);
         if (build == null) {
             return false;
         }
--- sonar-plugin-server/src/main/resources/buildServerResources/buildSummary.jsp
+++ sonar-plugin-server/src/main/resources/buildServerResources/buildSummary.jsp
@@ -1,5 +1,10 @@
-<tr>
-    <td class="st">
-        <a href="http://localhost:9000/dashboard/index/sonar-plugin">view in sonar</a>
-    </td>
-</tr>
+<%@ taglib prefix="util" uri="/WEB-INF/functions/util" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<%--@elvariable id="sonar_url" type="java.lang.String"--%>
+<c:if test="${not empty sonar_url}">
+    <tr>
+        <td class="st">
+            <a href="${util:escapeUrlForQuotes(sonar_url)}">view in sonar</a>
+        </td>
+    </tr>
+</c:if>
