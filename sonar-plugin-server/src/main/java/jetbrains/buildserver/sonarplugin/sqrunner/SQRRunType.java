45a1d10696367d8feef3e7c6bec1f78eeab2cb85
Linfar <tivof_andrey@mail.ru>
2014-04-08 15:13:29 +0400

    Editable Sonar parameters on buildtype page

--- sonar-plugin-server/src/main/java/jetbrains/buildserver/sonarplugin/sqrunner/SQRRunType.java
+++ sonar-plugin-server/src/main/java/jetbrains/buildserver/sonarplugin/sqrunner/SQRRunType.java
@@ -1,5 +1,6 @@
 package jetbrains.buildserver.sonarplugin.sqrunner;
 
+import jetbrains.buildServer.serverSide.InvalidProperty;
 import jetbrains.buildServer.serverSide.PropertiesProcessor;
 import jetbrains.buildServer.serverSide.RunType;
 import jetbrains.buildServer.serverSide.RunTypeRegistry;
@@ -7,6 +8,9 @@ import jetbrains.buildserver.sonarplugin.Constants;
 import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.HashMap;
 import java.util.Map;
 
 /**
@@ -19,6 +23,10 @@ public class SQRRunType extends RunType {
     private static final String VIEW_SQRRUN_PARAMS_JSP_PATH = "viewSQRRunParams.jsp";
     private static final String RUNNER_DISPLAY_NAME = "SonarQube Runner";
     private static final String RUNNER_DESCRIPTION = "Runner for SonarQube Runner";
+    public static final String DEFAULT_PROJECT_NAME = "%system.teamcity.projectName%";
+    public static final String DEFAULT_PROJECT_KEY = "%teamcity.project.id%";
+    public static final String DEFAULT_PROJECT_VERSION = "%build.number%";
+    public static final String DEFAULT_SOURCE_PATH = "src";
 
     public SQRRunType(final @NotNull RunTypeRegistry runTypeRegistry) {
         runTypeRegistry.registerRunType(this);
@@ -45,7 +53,11 @@ public class SQRRunType extends RunType {
     @Nullable
     @Override
     public PropertiesProcessor getRunnerPropertiesProcessor() {
-        return null;
+        return new PropertiesProcessor() {
+            public Collection<InvalidProperty> process(Map<String, String> properties) {
+                return Collections.emptyList();
+            }
+        };
     }
 
     @Nullable
@@ -63,6 +75,11 @@ public class SQRRunType extends RunType {
     @Nullable
     @Override
     public Map<String, String> getDefaultRunnerProperties() {
-        return null;
+        final Map<String, String> map = new HashMap<String, String>();
+        map.put(Constants.SONAR_PROJECT_NAME, DEFAULT_PROJECT_NAME);
+        map.put(Constants.SONAR_PROJECT_KEY, DEFAULT_PROJECT_KEY);
+        map.put(Constants.SONAR_PROJECT_VERSION, DEFAULT_PROJECT_VERSION);
+        map.put(Constants.SONAR_PROJECT_SOURCES, DEFAULT_SOURCE_PATH);
+        return map;
     }
 }
--- sonar-plugin-server/src/main/resources/buildServerResources/editSQRRunParams.jsp
+++ sonar-plugin-server/src/main/resources/buildServerResources/editSQRRunParams.jsp
@@ -1,2 +1,28 @@
-edit
-gogogo
+<%@ taglib prefix="props" tagdir="/WEB-INF/tags/props" %>
+<%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+<jsp:useBean id="propertiesBean" scope="request" type="jetbrains.buildServer.controllers.BasePropertiesBean"/>
+<tr>
+    <th class="noBorder"><label for="sonarProjectName">Project name: </label></th>
+    <td><props:textProperty name="sonarProjectName" className="longField"/>
+    </td>
+</tr>
+<tr>
+    <th class="noBorder"><label for="sonarProjectKey">Project key: </label></th>
+    <td><props:textProperty name="sonarProjectKey" className="longField"/>
+    </td>
+</tr>
+<tr>
+    <th class="noBorder"><label for="sonarProjectVersion">Project version: </label></th>
+    <td><props:textProperty name="sonarProjectVersion" className="longField"/>
+    </td>
+</tr>
+<tr>
+    <th class="noBorder"><label for="sonarProjectSources">Sources location: </label></th>
+    <td><props:textProperty name="sonarProjectSources" className="longField"/>
+    </td>
+</tr>
+<tr>
+    <th class="noBorder"><label for="sonarProjectModules">Modules: </label></th>
+    <td><props:textProperty name="sonarProjectModules" className="longField"/>
+    </td>
+</tr>
