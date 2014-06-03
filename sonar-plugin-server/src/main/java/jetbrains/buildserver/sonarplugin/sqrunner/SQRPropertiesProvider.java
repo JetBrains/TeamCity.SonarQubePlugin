af59648425b367d6231442779a3000390f0a292e
Linfar <tivof_andrey@mail.ru>
2014-06-03 15:05:38 +0400

    Nullability check added

--- sonar-plugin-server/src/main/java/jetbrains/buildserver/sonarplugin/sqrunner/SQRPropertiesProvider.java
+++ sonar-plugin-server/src/main/java/jetbrains/buildserver/sonarplugin/sqrunner/SQRPropertiesProvider.java
@@ -5,6 +5,7 @@ import jetbrains.buildserver.sonarplugin.Constants;
 import jetbrains.buildserver.sonarplugin.sqrunner.manager.SQSInfo;
 import jetbrains.buildserver.sonarplugin.sqrunner.manager.SQSManager;
 import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
 
 /**
  * Created by linfar on 6/2/14.
@@ -29,14 +30,22 @@ public class SQRPropertiesProvider implements BuildStartContextProcessor {
                     if (project != null) {
                         final SQSInfo server = mySqsManager.findServer(project, serverId);
                         if (server != null) {
-                            runnerContext.addRunnerParameter(Constants.SONAR_HOST_URL, server.getUrl());
-                            runnerContext.addRunnerParameter(Constants.SONAR_SERVER_JDBC_URL, server.getJDBCUrl());
-                            runnerContext.addRunnerParameter(Constants.SONAR_SERVER_JDBC_USERNAME, server.getJDBCUsername());
-                            runnerContext.addRunnerParameter(Constants.SONAR_SERVER_JDBC_PASSWORD, server.getJDBCPassword());
+                            addIfNotNull(runnerContext, Constants.SONAR_HOST_URL, server.getUrl());
+                            addIfNotNull(runnerContext, Constants.SONAR_SERVER_JDBC_URL, server.getJDBCUrl());
+                            addIfNotNull(runnerContext, Constants.SONAR_SERVER_JDBC_USERNAME, server.getJDBCUsername());
+                            addIfNotNull(runnerContext, Constants.SONAR_SERVER_JDBC_PASSWORD, server.getJDBCPassword());
                         }
                     }
                 }
             }
+        }
+    }
+
+    private static void addIfNotNull(final @NotNull SRunnerContext runnerContext,
+                                     final @NotNull String key,
+                                     final @Nullable String value) {
+        if (value != null) {
+            runnerContext.addRunnerParameter(key, value);
         }
     }
 }
