51a203937e21f5f22087c7c2139143b847366022
Linfar <tivof_andrey@mail.ru>
2015-07-01 16:38:27 +0300

    minor refactoring

--- sonar-plugin-server/src/main/java/jetbrains/buildserver/sonarplugin/sqrunner/SQSPropertiesProvider.java
+++ sonar-plugin-server/src/main/java/jetbrains/buildserver/sonarplugin/sqrunner/SQSPropertiesProvider.java
@@ -40,11 +40,11 @@ public class SQSPropertiesProvider implements BuildStartContextProcessor {
                             addIfNotNull(runnerContext, Constants.SONAR_LOGIN, server.getLogin());
                             addIfNotNull(runnerContext, Constants.SONAR_SERVER_JDBC_URL, server.getJDBCUrl());
                             addIfNotNull(runnerContext, Constants.SONAR_SERVER_JDBC_USERNAME, server.getJDBCUsername());
-                            if (!StringUtil.isEmpty(context.getSharedParameters().get(Constants.SECURE_TEAMCITY_PASSWORD_PREFIX + Constants.SONAR_PASSWORD))) {
-                                runnerContext.addRunnerParameter(Constants.SONAR_PASSWORD, "%" + Constants.SECURE_TEAMCITY_PASSWORD_PREFIX + Constants.SONAR_PASSWORD + "%");
+                            if (!StringUtil.isEmpty(context.getSharedParameters().get(getSecuredPropertyName(Constants.SONAR_PASSWORD)))) {
+                                runnerContext.addRunnerParameter(Constants.SONAR_PASSWORD, "%" + getSecuredPropertyName(Constants.SONAR_PASSWORD) + "%");
                             }
-                            if (!StringUtil.isEmpty(context.getSharedParameters().get(Constants.SECURE_TEAMCITY_PASSWORD_PREFIX + Constants.SONAR_SERVER_JDBC_PASSWORD))) {
-                                runnerContext.addRunnerParameter(Constants.SONAR_SERVER_JDBC_PASSWORD, "%" + Constants.SECURE_TEAMCITY_PASSWORD_PREFIX + Constants.SONAR_SERVER_JDBC_PASSWORD + "%");
+                            if (!StringUtil.isEmpty(context.getSharedParameters().get(getSecuredPropertyName(Constants.SONAR_SERVER_JDBC_PASSWORD)))) {
+                                runnerContext.addRunnerParameter(Constants.SONAR_SERVER_JDBC_PASSWORD, "%" + getSecuredPropertyName(Constants.SONAR_SERVER_JDBC_PASSWORD) + "%");
                             }
 //                            break;
                         }
@@ -52,6 +52,11 @@ public class SQSPropertiesProvider implements BuildStartContextProcessor {
                 }
             }
         }
+    }
+
+    @NotNull
+    private String getSecuredPropertyName(final @NotNull String key) {
+        return Constants.SECURE_TEAMCITY_PASSWORD_PREFIX + key;
     }
 
     private static void addIfNotNull(final @NotNull SRunnerContext runnerContext,
