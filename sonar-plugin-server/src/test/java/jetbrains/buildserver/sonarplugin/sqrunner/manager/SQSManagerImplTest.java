d8485ac32e17354d93e30254bc1142f0e592fc81
linfar <andrey.titov@jetbrains.com>
2016-09-30 15:59:55 +0300

    Init testing for server part

---
+++ sonar-plugin-server/src/test/java/jetbrains/buildserver/sonarplugin/sqrunner/manager/SQSManagerImplTest.java
@@ -0,0 +1,10 @@
+package jetbrains.buildserver.sonarplugin.sqrunner.manager;
+
+import org.testng.annotations.Test;
+
+/**
+ * Created by Andrey Titov on 30.09.16.
+ */
+@Test
+public class SQSManagerImplTest {
+}
