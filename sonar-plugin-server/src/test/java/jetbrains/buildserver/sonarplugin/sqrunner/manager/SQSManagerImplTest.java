1c8d7cb0d22e6fe9e9e5325156784c7a22fe3c39
linfar <andrey.titov@jetbrains.com>
2016-10-03 15:11:22 +0300

    More tests

--- sonar-plugin-server/src/test/java/jetbrains/buildserver/sonarplugin/sqrunner/manager/SQSManagerImplTest.java
+++ sonar-plugin-server/src/test/java/jetbrains/buildserver/sonarplugin/sqrunner/manager/SQSManagerImplTest.java
@@ -2,9 +2,10 @@ package jetbrains.buildserver.sonarplugin.sqrunner.manager;
 
 import jetbrains.buildServer.serverSide.SProject;
 import jetbrains.buildServer.serverSide.settings.ProjectSettingsManager;
+import org.assertj.core.api.Condition;
+import org.jetbrains.annotations.NotNull;
+import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
-
-import java.util.List;
 
 import static org.assertj.core.api.BDDAssertions.then;
 import static org.mockito.Mockito.mock;
@@ -15,27 +16,80 @@ import static org.mockito.Mockito.when;
  */
 @Test
 public class SQSManagerImplTest {
-    public void test_simple_own() {
-        final ProjectSettingsManager settingsManager = mock(ProjectSettingsManager.class);
-        final String projectId = "projectId";
+    private SProject myRoot;
+    private SProject myProject;
+    private XMLBasedSQSInfo myRootServerInfo;
+    private XMLBasedSQSInfo myServerInfo;
+    private ProjectSettingsManager mySettingsManager;
+    @NotNull private final String myProjectId = "projectId";
+    @NotNull private final String myRootProjectId = "_Root";
+    @NotNull private final String myRootServerId= "rootServerId";
+    @NotNull private final String myServerId = "serverId";
 
+    @BeforeMethod(alwaysRun = true)
+    public void setUp() throws Exception {
+        myRoot = mock(SProject.class);
+        when(myRoot.getProjectId()).thenReturn(myRootProjectId);
+        when(myRoot.getParentProject()).thenReturn(null);
+
+        myProject = mock(SProject.class);
+        when(myProject.getProjectId()).thenReturn(myProjectId);
+        when(myProject.getParentProject()).thenReturn(myRoot);
+
+        mySettingsManager = mock(ProjectSettingsManager.class);
+
+        myServerInfo = mockSQSInfo(mySettingsManager, myServerId, myProjectId);
+
+        myRootServerInfo = mockSQSInfo(mySettingsManager, myRootServerId, myRootProjectId);
+    }
+
+    public void test_own() {
+        final SQSManagerImpl sqsManager = new SQSManagerImpl(mySettingsManager);
+
+        then(sqsManager.getAvailableServers(SQSManager.ProjectAccessor.single(myProject)))
+                .hasSize(1)
+                .areExactly(1, new Condition<>(sqsInfo -> sqsInfo.getId().equals(myServerId), "Should be one " + myServerId));
+        then(sqsManager.getAvailableServers(SQSManager.ProjectAccessor.single(myRoot)))
+                .hasSize(1)
+                .areExactly(1, new Condition<>(sqsInfo -> sqsInfo.getId().equals(myRootServerId), "Should be one " + myRootServerId));
+
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.single(myProject), myServerId)).isNotNull().isSameAs(myServerInfo);
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.single(myProject), "nonExisting")).isNull();
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.single(myProject), myRootServerId)).isNull();
+
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.single(myRoot), myServerId)).isNull();
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.single(myRoot), "nonExisting")).isNull();
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.single(myRoot), myRootServerId)).isNotNull().isSameAs(myRootServerInfo);
+    }
+
+    public void test_recurse() {
+        final SQSManagerImpl sqsManager = new SQSManagerImpl(mySettingsManager);
+
+        then(sqsManager.getAvailableServers(SQSManager.ProjectAccessor.recurse(myProject)))
+                .hasSize(2)
+                .areExactly(1, new Condition<>(sqsInfo -> sqsInfo.getId().equals(myServerId), "Should be one " + myServerId))
+                .areExactly(1, new Condition<>(sqsInfo -> sqsInfo.getId().equals(myRootServerId), "Should be one " + myServerId));
+        then(sqsManager.getAvailableServers(SQSManager.ProjectAccessor.single(myRoot)))
+                .hasSize(1)
+                .areExactly(1, new Condition<>(sqsInfo -> sqsInfo.getId().equals(myRootServerId), "containing %s", myRootServerId));
+
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.recurse(myProject), myServerId)).isNotNull().isSameAs(myServerInfo);
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.recurse(myProject), "nonExisting")).isNull();
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.recurse(myProject), myRootServerId)).isNotNull().isSameAs(myRootServerInfo);
+
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.recurse(myRoot), myServerId)).isNull();
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.recurse(myRoot), "nonExisting")).isNull();
+        then(sqsManager.findServer(SQSManager.ProjectAccessor.recurse(myRoot), myRootServerId)).isNotNull().isSameAs(myRootServerInfo);
+    }
+
+    @NotNull
+    private static XMLBasedSQSInfo mockSQSInfo(@NotNull final ProjectSettingsManager settingsManager,
+                                               @NotNull final String serverId,
+                                               @NotNull final String inProject) {
         final SQSProjectSettings t = new SQSProjectSettings();
-        final String serverId = "serverId";
         final XMLBasedSQSInfo modifiedServer = new XMLBasedSQSInfo(serverId, null, null, null, null, null, null, null);
         t.setInfo(serverId, modifiedServer);
-
-        when(settingsManager.getSettings(projectId, SQSManagerImpl.SQS_MANAGER_KEY)).thenReturn(t);
-
-        final SProject project = mock(SProject.class);
-        when(project.getProjectId()).thenReturn(projectId);
-
-        final SQSManagerImpl sqsManager = new SQSManagerImpl(settingsManager);
-
-        final List<SQSInfo> availableServers = sqsManager.getAvailableServers(SQSManager.ProjectAccessor.single(project));
-        then(availableServers).hasSize(1);
-        then(availableServers.get(0).getId()).isSameAs(serverId);
-
-        then(sqsManager.findServer(SQSManager.ProjectAccessor.single(project), serverId)).isNotNull().isSameAs(modifiedServer);
-        then(sqsManager.findServer(SQSManager.ProjectAccessor.single(project), "nonExisting")).isNull();
+        when(settingsManager.getSettings(inProject, SQSManagerImpl.SQS_MANAGER_KEY)).thenReturn(t);
+        return modifiedServer;
     }
 }
