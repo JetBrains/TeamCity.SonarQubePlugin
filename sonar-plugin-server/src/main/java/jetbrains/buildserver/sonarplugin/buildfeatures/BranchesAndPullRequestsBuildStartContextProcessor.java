285ea14282192bd128c26df8743e8fc558256094
Alix Lourme <alix.lourme@gmail.com>
2020-09-09 18:22:48 +0200

    Fix #82 : SQ Branch&PR support : switch to BuildStartContextProcessor

--- sonar-plugin-server/src/main/java/jetbrains/buildserver/sonarplugin/buildfeatures/BranchesAndPullRequestsBuildStartContextProcessor.java
+++ sonar-plugin-server/src/main/java/jetbrains/buildserver/sonarplugin/buildfeatures/BranchesAndPullRequestsBuildStartContextProcessor.java
@@ -16,15 +16,14 @@
 
 package jetbrains.buildserver.sonarplugin.buildfeatures;
 
-import java.util.Map;
-
 import org.springframework.util.StringUtils;
 
 import com.google.gson.JsonObject;
 
+import jetbrains.buildServer.agent.Constants;
 import jetbrains.buildServer.messages.Status;
-import jetbrains.buildServer.serverSide.ParametersPreprocessor;
-import jetbrains.buildServer.serverSide.SRunningBuild;
+import jetbrains.buildServer.serverSide.BuildStartContext;
+import jetbrains.buildServer.serverSide.BuildStartContextProcessor;
 import jetbrains.buildServer.serverSide.buildLog.MessageAttrs;
 import jetbrains.buildServer.version.ServerVersionHolder;
 import jetbrains.buildServer.version.ServerVersionInfo;
@@ -32,27 +31,28 @@ import jetbrains.buildServer.version.ServerVersionInfo;
 /**
  * Provides SONARQUBE_SCANNER_PARAMS environment variable on build if feature activated
  */
-public class BranchesAndPullRequestsParametersPreprocessor implements ParametersPreprocessor {
+public class BranchesAndPullRequestsBuildStartContextProcessor implements BuildStartContextProcessor {
 
-    static final String SQS_SYSENV = "env.SONARQUBE_SCANNER_PARAMS";
+    static final String SQS_SYSENV = Constants.ENV_PREFIX + "SONARQUBE_SCANNER_PARAMS";
 
     @Override
-    public void fixRunBuildParameters(SRunningBuild build, Map<String, String> runParameters, Map<String, String> buildParams) {
+    public void updateParameters(BuildStartContext context) {
 
-        if (build.getBuildFeaturesOfType(BranchesAndPullRequestsBuildFeature.BUILD_FEATURE_TYPE).isEmpty() || buildParams.containsKey(SQS_SYSENV)) {
+        if (context.getBuild().getBuildFeaturesOfType(BranchesAndPullRequestsBuildFeature.BUILD_FEATURE_TYPE).isEmpty()
+                || context.getSharedParameters().containsKey(SQS_SYSENV)) {
             // Currently only GitHub is supported => if feature defined, it is for GitHub
-            // ParametersPreprocessor is called for each steps (or could be defined manually). So if sysenv already defined, skip process.
+            // If sysenv already defined (outside this processor), skip process.
             return;
         }
 
         if (!isTeamCityMinimalVersion(getServerVersionInfo())) {
-            build.getBuildLog().message(
-                    String.format("Build feature '%s' requiers TeamCity 2019.2 or above", BranchesAndPullRequestsBuildFeature.BUILD_FEATURE_NAME),
+            context.getBuild().getBuildLog().message(
+                    String.format("Build feature '%s' requires TeamCity 2019.2 or above", BranchesAndPullRequestsBuildFeature.BUILD_FEATURE_NAME),
                     Status.ERROR, MessageAttrs.attrs());
             return;
         }
 
-        String branchIsDefault = buildParams.get("teamcity.build.branch.is_default");
+        String branchIsDefault = context.getSharedParameters().get("teamcity.build.branch.is_default");
         if (StringUtils.isEmpty(branchIsDefault) || Boolean.TRUE.equals(Boolean.valueOf(branchIsDefault))) {
             // No information or default branch, nothing to provide
             return;
@@ -60,32 +60,32 @@ public class BranchesAndPullRequestsParametersPreprocessor implements Parameters
 
         final String type;
         final JsonObject json = new JsonObject();
-        final String prNumber = buildParams.get("teamcity.pullRequest.number");
+        final String prNumber = context.getSharedParameters().get("teamcity.pullRequest.number");
         if (StringUtils.isEmpty(prNumber)) {
             // Branch
             type = "branch";
-            final String vcsBranch = buildParams.get("vcsroot.branch");
-            json.addProperty("sonar.branch.name", buildParams.get("teamcity.build.branch"));
+            final String vcsBranch = context.getSharedParameters().get("vcsroot.branch");
+            json.addProperty("sonar.branch.name", context.getSharedParameters().get("teamcity.build.branch"));
             json.addProperty("sonar.branch.target", vcsBranch.substring(vcsBranch.indexOf("refs/heads/") + 11));
         } else {
             // Pull Request
             type = "pull-request";
-            String repo = buildParams.get("vcsroot.url");
+            String repo = context.getSharedParameters().get("vcsroot.url");
             repo = repo.replaceFirst("^git@.*:", "");
             repo = repo.replaceFirst("^https?://[^/]*/", "");
             repo = repo.replaceFirst("\\.git$", "");
 
             json.addProperty("sonar.pullrequest.key", prNumber);
-            json.addProperty("sonar.pullrequest.branch", buildParams.get("teamcity.pullRequest.title"));
-            json.addProperty("sonar.pullrequest.base", buildParams.get("teamcity.pullRequest.target.branch"));
+            json.addProperty("sonar.pullrequest.branch", context.getSharedParameters().get("teamcity.pullRequest.title"));
+            json.addProperty("sonar.pullrequest.base", context.getSharedParameters().get("teamcity.pullRequest.target.branch"));
             json.addProperty("sonar.pullrequest.provider", "github");
             json.addProperty("sonar.pullrequest.github.repository", repo);
         }
 
         final String jsonString = json.toString();
-        build.getBuildLog().message(String.format("SonarQube plugin detects %s, '%s' set with '%s'", type, SQS_SYSENV, jsonString), Status.NORMAL,
-                MessageAttrs.attrs());
-        buildParams.put(SQS_SYSENV, jsonString);
+        context.getBuild().getBuildLog().message(String.format("SonarQube plugin detects %s, '%s' set with '%s'", type, SQS_SYSENV, jsonString),
+                Status.NORMAL, MessageAttrs.attrs());
+        context.addSharedParameter(SQS_SYSENV, jsonString);
     }
 
     protected ServerVersionInfo getServerVersionInfo() {
